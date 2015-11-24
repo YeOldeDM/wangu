3587efe4ab582e0e9e5e157af1f41efb9614eb7e
ian <nines@email.com>
2015-11-23 18:36:13 -0700

    format_numbers for combat info

--- formats.gd
+++ formats.gd
@@ -79,3 +79,29 @@ func _number(n):
 	else:
 		n = str(n)
 	return str(n,suffix)
+
+var greek_abc = [
+	"Alpha",
+	"Beta",
+	"Gamma",
+	"Delta",
+	"Epsilon",
+	"Zeta",
+	"Eta",
+	"Theta",
+	"Iota",
+	"Kappa",
+	"Lambda",
+	"Mu",
+	"Nu",
+	"Xi",
+	"Omicron",
+	"Pi",
+	"Rho",
+	"Sigma",
+	"Tau",
+	"Upsilon",
+	"Phi",
+	"Chi",
+	"Psi",
+	"Omega"]
--- scripts/combat.gd
+++ scripts/combat.gd
@@ -1,11 +1,15 @@
 
 extends Control
+var format
 
 class Army:
-	var troops = 1
+	var troops = 32
 	var damage = 1
 	var damage_var = 0.4
 	var unit_health = 5
+	
+	var weapons = 12
+	var armor = 34
 	var shields = 0
 	
 	var current_health = 0
@@ -14,7 +18,7 @@ class Army:
 		self.current_health = self.army_health()/2
 	
 	func army_damage():
-		var base_damage = self.damage * self.troops
+		var base_damage = (self.damage+self.weapons) * self.troops
 		var min_dmg = ceil(base_damage*damage_var)
 		var max_dmg = ceil(base_damage * (1.0+damage_var))
 		return [min_dmg,max_dmg]
@@ -25,14 +29,21 @@ class Army:
 		return randi(dmg[0],dmg[1])
 	
 	func army_health():
-		return self.unit_health * self.troops
+		return (self.unit_health+self.armor) * self.troops
 
 	func army_shields():
 		return self.shields * self.troops
 	
 	func army_get_hit(dmg):
 		var damage = max(0,dmg-self.army_defense())
-		self.current_health
+		var new_health = self.current_health - damage
+		if new_health <= 0:
+			self.die()
+		else:
+			self.current_health = new_health
+	
+	func die():
+		self.new_army()
 
 
 
@@ -40,13 +51,18 @@ var army
 var bots_panel
 var mob_panel
 
+var sector = 0
+var zone = 1
 
+func draw_map_info():
+	get_node('Battle/map/sector').set_text(str("Sector ",format.greek_abc[sector]))
+	get_node('Battle/map/zone').set_text(str("Zone ",str(zone)))
 
 func draw_bots_combat_info():
-	var troops = str("Troopers (",str(army.troops),")")
+	var troops = str("Troopers (",format._number(army.troops),")")
 	var d = army.army_damage()
-	var damage = str("Damage: ",str(d[0]),"-",str(d[1]))
-	var shields = str("Shields: ",str(army.army_shields()))
+	var damage = str("Damage: ",format._number(d[0]),"-",format._number(d[1]))
+	var shields = str("Shields: ",format._number(army.army_shields()))
 	
 	bots_panel.get_node('troops').set_text(troops)
 	bots_panel.get_node('damage').set_text(damage)
@@ -62,19 +78,22 @@ func check_bots_healthbar(per):
 	else:
 		bar_per += sign(per-bar_per) * (abs(per-bar_per)*0.05)
 		bar.set_value(bar_per)
-	bar.get_node('health').set_text(str(str(army.current_health),"/",str(army.army_health())))
+	bar.get_node('health').set_text(str(format._number(army.current_health),"/",format._number(army.army_health())))
 
 
 
 
 
 func _ready():
+	format = get_node('/root/formats')
+	
 	army = Army.new()
 	army.new_army()
 	bots_panel = get_node('Battle/cont/bots')
 	mob_panel = get_node('Battle/cont/mobs')
 	
 	draw_bots_combat_info()
+	
 	
 	set_process(true)
 
