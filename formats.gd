ff3ea294a47ed97383d00ac732dd375933ec78ee
YeOldeDM <nines@email.com>
2015-12-02 13:01:03 -0700

    add support for trillions in format._number()

--- formats.gd
+++ formats.gd
@@ -1,6 +1,7 @@
 extends Node
 
 func _verbose_time(t):
+	#convert t sec into "ad, xh, ym, zs" format
 	var seconds = t
 	var minutes = int(t/60)
 	var hours = int(minutes / 60)
@@ -32,7 +33,7 @@ func _verbose_time(t):
 
 func _time(t):
 	#format t seconds into hr:mn:sc format
-	var seconds = t
+	var seconds = int(t)
 	var minutes = int(t/60)
 	var hours = int(minutes / 60)
 	var days = int(hours / 24)
@@ -60,16 +61,21 @@ func _time(t):
 	
 	
 func _number(n):
+	#Attempt to clamp a number to a max of 6 characters
 	var suffix = ''
-	if n >= 1000000000:
+	if n >= 1000000000000:			#Trillions (1,000b)
+		n = str(n*0.000000000001).left(5)
+		suffix = 't'
+	elif n >= 1000000000:			#Billions (1,000m)
 		n = str(n*0.000000001).left(5)
 		suffix = 'b'
-	elif n >= 1000000:
+	elif n >= 1000000:				#Millions (1,000k)
 		n = str(n*0.000001).left(5)
-		suffix='m'
-	elif n >= 10000:
+		suffix = 'm'
+	elif n >= 10000:				#Thousands
 		n = str(n*0.001).left(5)
 		suffix = 'k'
+	#Less than 10,000...
 	elif n <= 9:
 		n = str(n).left(4)
 	elif n <= 99:
@@ -78,6 +84,7 @@ func _number(n):
 		n = str(int(n)).left(6)
 	else:
 		n = str(n)
+	#return number+suffix
 	return str(n,suffix)
 
 var roman_numerals = [
--- main.gd
+++ main.gd
@@ -1,23 +1,34 @@
 
 extends Node
 
+var format
+
 var bank
 var population
 var combat
 var news
 var construction
 
+var time_label
+var game_time = 0.0
+
 func _ready():
+	format = get_node('/root/formats')
 	#master links
 	bank = get_node('Bank')
 	population = get_node('population')
 	combat= get_node('combat')
 	news = get_node('news')
 	construction = get_node('construction')
+	time_label = get_node('sys_panel/time')
+	
 	set_process(true)
 
 
 func _process(delta):
+	game_time += delta
+	var t = format._time(game_time)
+	time_label.set_text(str("Time: ",t))
 	#BANK
 	bank.process(delta)
 	#POPULATION
